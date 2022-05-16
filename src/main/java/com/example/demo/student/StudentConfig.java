d4969e048282383158e0f63b43ba1cde577ad1b0
Missael Aguilar <dev.missael@gmail.com>
2022-05-16 15:55:14 -0500

    First commit

---
+++ src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,30 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository) {
+        return args -> {
+            Student esther = new Student(1L,
+                "Esther",
+                "esther@gmail.com",
+                LocalDate.of(2000, Month.JANUARY, 5));
+
+            Student alex = new Student(
+                    "Alex",
+                    "alex@gmail.com",
+                    LocalDate.of(2004, Month.JANUARY, 5));
+
+            repository.saveAll(List.of(esther, alex));
+        };
+    }
+}
